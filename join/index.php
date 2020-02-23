299e3651ad718f8b3306695a9cb19547769ae753
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2020-02-23 15:43:42 +0900

    Update index.php

--- join/index.php
+++ join/index.php
@@ -1,7 +1,26 @@
 <?php
-if ($_POST['name'] === ''){
+if (!empty($_POST)) {
+	if ($_POST['name'] === ''){
 		$error['name'] = 'blank';
+	}
+	if ($_POST['email'] === ''){
+		$error['email'] = 'blank';
+	}
+	if ($_POST['password'] === ''){
+		$error['password'] = 'blank';
+	}
+	if (strlen($_POST['password']) < 4 ){
+		$error['password'] = 'length';
+	}
+
+	if (empty($error)) {
+		header('Location: check.php');
+		exit();
+	}
 }
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -25,17 +44,26 @@ if ($_POST['name'] === ''){
 	<dl>
 		<dt>ニックネーム<span class="required">必須</span></dt>
 		<dd>
-        	<input type="text" name="name" size="35" maxlength="255" value="" />
+        	<input type="text" name="name" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['name'],ENT_QUOTES)); ?>" />
 					<?php if ($error['name'] == 'blank'):?>
-						<p class="error">メールアドレスが入力されていません。</p>
+						<p class="error">ニックネームが入力されていません。</p>
 					<?php endif; ?>
 		</dd>
 		<dt>メールアドレス<span class="required">必須</span></dt>
 		<dd>
-        	<input type="text" name="email" size="35" maxlength="255" value="" />
+				<input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['email'],ENT_QUOTES)); ?>" />
+				<?php if ($error['email'] == 'blank'):?>
+					<p class ="error">メールアドレスが入力されていません</p>
+				<?php endif; ?>
 		<dt>パスワード<span class="required">必須</span></dt>
 		<dd>
-        	<input type="password" name="password" size="10" maxlength="20" value="" />
+        	<input type="password" name="password" size="10" maxlength="20" value="<?php print(htmlspecialchars($_POST['password'],ENT_QUOTES)); ?>" />
+					<?php if ($error['password'] == 'blank'): ?>
+						<p class = "error">パスワードを入力して下さい</p>
+					<?php endif; ?>
+					<?php if ($error['password'] == 'length'): ?>
+						<p class = "error">４文字以上のパスワードを入力して下さい。</p>
+					<?php endif; ?>
         </dd>
 		<dt>写真など</dt>
 		<dd>
